43408845bee1742e95b3982851d005ebd65881c3
Micha Zegan <michal.zeg@gmail.com>
2016-03-02 19:04:10 +0100

    Corrected Drawing Panel

--- SectionsEC/Windows/CommonWindows/DrawingPage.xaml.cs
+++ SectionsEC/Windows/CommonWindows/DrawingPage.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SectionsEC.Drawing;
+using SectionsEC.Helpers;
 using GalaSoft.MvvmLight.Messaging;
 
 namespace SectionsEC.Views
@@ -21,10 +12,42 @@ namespace SectionsEC.Views
     /// </summary>
     public partial class DrawingPage : UserControl
     {
+        private PerimeterProperties perimeterProperties;
+        //private Grid canvas;
+        private SectionDrawing sectionDrawing;
+        private BarsDrawing barsDrawing;
+
+
         public DrawingPage()
         {
             InitializeComponent();
-            Messenger.Default.Send<Grid>(this.canvas);
+            Messenger.Default.Register<IList<PointD>>(this, updatePerimeter);
+            Messenger.Default.Register<IList<Bar>>(this, updateBars);
+            //Messenger.Default.Send<Grid>(this.canvas);
+            createCanvas();
+        }
+        private void createCanvas()
+        {
+            //this.canvas = canvas;
+            this.perimeterProperties = new PerimeterProperties(() => canvas.ActualWidth, () => canvas.ActualHeight);
+            this.sectionDrawing = new SectionDrawing(this.canvas, this.perimeterProperties);
+            this.barsDrawing = new BarsDrawing(this.canvas, this.perimeterProperties);
+        }
+
+        private void updatePerimeter(IList<PointD> perimeter)
+        {
+            sectionDrawing.Perimeter(perimeter);
+        }
+        private void updateBars(IList<Bar> bars)
+        {
+            barsDrawing.Bars(bars);
+        }
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            this.perimeterProperties.UpdateProperties();
+            this.sectionDrawing.Redraw();
+            this.barsDrawing.Redraw();
         }
     }
 }
--- SectionsEC/Windows/CustomSectionWindow/CustomSectionWindow.xaml.cs
+++ SectionsEC/Windows/CustomSectionWindow/CustomSectionWindow.xaml.cs
@@ -23,43 +23,29 @@ namespace SectionsEC.Views
     /// </summary>
     public partial class CustomWindow : Window
     {
-        private PerimeterProperties perimeterProperties;
-        private Grid canvas;
-        private SectionDrawing sectionDrawing;
-        private BarsDrawing barsDrawing;
-
         public CustomWindow()
         {
             InitializeComponent();
             this.DataContext = new CustomSectionWindowViewModel();
-            Messenger.Default.Register<Grid>(this, createCanvas);
+            //Messenger.Default.Register<Grid>(this, createCanvas);
         }
 
-        private void createCanvas(Grid canvas)
-        {
-            this.canvas = canvas;
-            this.perimeterProperties = new PerimeterProperties(() => canvas.ActualWidth, () => canvas.ActualHeight);
-            this.sectionDrawing = new SectionDrawing(this.canvas, this.perimeterProperties);
-            this.barsDrawing = new BarsDrawing(this.canvas, this.perimeterProperties);
-        }
+        
 
         private void DataGrid_TargetUpdated(object sender, DataTransferEventArgs e)
         {
             var sectionCoordinates = ((IEnumerable<PointD>)this.dataGridPoints.ItemsSource).ToList();
             //if (sectionCoordinates.Count!=0)
-                sectionDrawing.Perimeter(sectionCoordinates);
+            //sectionDrawing.Perimeter(sectionCoordinates);
+            Messenger.Default.Send<IList<PointD>>(sectionCoordinates);
 
             var barData = ((IEnumerable<Bar>)this.dataGridBars.ItemsSource).ToList();
             //if (barData.Count != 0)
-                barsDrawing.Bars(barData);
+            //barsDrawing.Bars(barData);
+            Messenger.Default.Send<IList<Bar>>(barData);
 
         }
 
-        private void ContentControl_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            this.perimeterProperties.UpdateProperties();
-            this.sectionDrawing.Redraw();
-            this.barsDrawing.Redraw();
-        }
+        
     }
 }
